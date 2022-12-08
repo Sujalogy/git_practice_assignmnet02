56c62d612bc8b746bbaf3fc926ab52b80442d69f
rupakmandal522 <rupakmandal522@gmail.com>
2022-12-08 19:19:06 +0530

    add

--- prime.js
+++ prime.js
@@ -1,16 +1,17 @@
 // this is the prime number function ;
 
 function prime(n) {
+	let count = 0
 	for(let i = 0; i <= n; i++) {
-		if(i % n === 0) {
+		if(n%i === 0) {
 		count++;
 	    }
 	}
 	  
-	if(count === 2) {
-     console.log(Yes);
+	if(count <= 2) {
+     console.log("Yes");
     } else {
-     console.log(No);
+     console.log("No");
     }
 }
 
